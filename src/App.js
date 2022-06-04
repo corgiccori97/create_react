1dff4b2121f400110cf2ca34085d368d1eca6d4e
corgiccori97 <tkdgmlgg7@gmail.com>
2022-06-04 16:24:29 +0900

    deps

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import Button from "./Button";
+// import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState();
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("i run all the time");
+  // rerender 될때마다 매번 실행시키고 싶지 않을 때 딱 한 번만 실행(이를테면 api 호출이라던가)
+  useEffect(() => {
+    console.log("CALL THE API...");
+  }, []);
   return (
     <div>
-      <h1 className={styles.title}>WELCOME BACK</h1>
-      <Button text={"continue"} />
+      <h1 className={styles.title}>WELCOME</h1>
+      <input value={keyword} onChange={onChange} placeholder="Search here" type="text" />
+      <h2>{counter}</h2>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
