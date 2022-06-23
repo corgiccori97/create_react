da279d0760309460b398f8b33e7a83fdbcde229e
corgiccori97 <tkdgmlgg7@gmail.com>
2022-06-23 23:18:05 +0900

    mamuri & publishing

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,6 +1,31 @@
+import { useEffect, useState } from 'react';
+import { useParams } from "react-router-dom";
+import JjinMovie from '../components/JjinMovie';
 function Detail() {
+    const [movie, setMovie] = useState('');
+    const { id } = useParams();
+    const getMovie = async () => {
+        const json = await (
+            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        ).json();
+        setMovie(json.data.movie);
+    };
+    useEffect(() => {
+        getMovie();
+    }, []);
+    console.log(movie);
+
     return (
-        <h1>Detail</h1>
+        <div>
+            <h1>Detail</h1>
+            <JjinMovie                            
+                key={id}
+                id={id}
+                coverImg={movie.medium_cover_image} 
+                title={movie.title} 
+                summary={movie.summary} 
+                genres={movie.genres} />
+        </div>
     );
 }
 
